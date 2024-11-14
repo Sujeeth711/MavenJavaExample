78fb9976233a512ee4ad3faab79cbeca6960a4ab
Sujeeth711 <sujeeth5711@gmail.com>
2024-11-14 16:03:58 +0530

    Added Maven Java Project files to repository

---
+++ src/main/java/com/project/demo/MavenJavaProj/App.java
@@ -0,0 +1,13 @@
+package com.project.demo.MavenJavaProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Sujeeth 22BD1A0525" );
+    }
+}
